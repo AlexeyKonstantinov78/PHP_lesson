4dc9b0c42843c3bc17928d76169e52755f34336c
Aleksey Konstantinov <alex_kav@mail.ru>
2021-03-02 22:03:52 +0300

    практика 2 портфолио в связке с бд

---
+++ portfolio_db/data.php
@@ -0,0 +1,62 @@
+<?php
+
+return [
+    'about' => [
+        'name' => 'Алексей Константинов',
+        'post' => 'WEB верстка',
+        'email' => 'alekseykonstantinov@yandex.ru',
+        'phone' => '89063835583',
+        'site' => 'alekseykonstantinov.ru',
+        'github' => 'github.com/AlexeyKonstantinov78'
+    ],
+    'education' => [
+        [
+            'faculty' => 'Электроэнергетика',
+            'university' => 'Чувашский государственный университет им. И.Н. Ульянова, Чебоксары',
+            'yearStart' => 1995,
+            'yearEnd' => 2001
+        ],
+        [
+            'faculty' => 'Управление государственными и муниципальными учреждениями',
+            'university' => 'ФГБОУ ВПО "ЧГУ им. И. Н. Ульянова"',
+            'yearStart' => 2012,
+            'yearEnd' => 2012
+        ],
+        [
+            'faculty' => 'Crash-курс по информационной безопасности',
+            'university' => 'АНО ВО "Университет Иннополис"',
+            'yearStart' => 2012,
+            'yearEnd' => 2020
+        ],
+    ],
+    'languages' => [
+        'Русский'
+    ],
+    'interests' => [
+        'компьютеры', 'авто', 'мастерить'
+    ],
+    'aboutCareer' => 'Lorem lorem lorem',
+    'career' => [
+        [
+            'post' => 'post1',
+            'plase' => 'Citi1',
+            'duty' => 'lorem lorem111',
+            'yearStart' => 2016,
+            'yearEnd' => 'настоящее время'
+        ],
+        [
+            'post' => 'post2',
+            'plase' => 'Citi2',
+            'duty' => 'lorem lorem222',
+            'yearStart' => 2010,
+            'yearEnd' => 2015
+        ],
+        [
+            'post' => 'post3',
+            'plase' => 'Citi3',
+            'duty' => 'lorem lorem333',
+            'yearStart' => 2005,
+            'yearEnd' => 2009
+        ],
+    ]
+];
